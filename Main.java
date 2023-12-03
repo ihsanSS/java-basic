d1ed06e0f5ef6018c1525bfa67d5e9f133d2cb5f
ihsanSS <ihsan.satria99@gmail.com>
2023-12-03 23:30:29 +0700

    Java basic struktur programing

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main{
 	public static void main(String[] args) {
-	System.out.println("Hello wolrd");
-	System.out.println("Ihsan suci satria");	
+	System.out.println("Hello wolrd");	
 	}
 }
